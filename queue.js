87c439421d43bc55fd825fa1e8b871f53af9b046
Aleksander Budzynowski <998324+aleksb@users.noreply.github.com>
2019-04-03 12:45:27 +1100

    a failing length test for queue

---
+++ queue.js
@@ -0,0 +1,21 @@
+/*
+ * Queue data structure (FIFO - first in first out)
+ *
+ * public methods:
+ *
+ *   add(i)    adds item i to the queue
+ *   remove()  removes an item from the queue and returns it
+ *   length()  returns current list length
+ */
+
+class Queue {
+
+    constructor() {
+    }
+
+    add(item) {
+    }
+}
+
+
+module.exports = Queue;
---
+++ queue.test.js
@@ -0,0 +1,12 @@
+const Queue = require('./queue');
+
+
+describe("test the length method", () => {
+
+    const q = new Queue();
+
+    test("list initially empty", () => {
+        expect(q.length().toBe(0));
+    });
+
+});
