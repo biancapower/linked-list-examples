9104d46b5256e68c3bf9df4ba5de5f3f86d4c01f
Aleksander Budzynowski <998324+aleksb@users.noreply.github.com>
2019-04-03 13:48:16 +1100

    developed length test batch and passing tests

--- queue.js
+++ queue.js
@@ -1,19 +1,36 @@
+"use strict";
 /*
  * Queue data structure (FIFO - first in first out)
  *
  * public methods:
  *
- *   add(i)    adds item i to the queue
- *   remove()  removes an item from the queue and returns it
+ *   add(i)    adds item i to the end queue
+ *   remove()  removes an item from the front of the queue
+ *             and returns it
+ *             - if there are no items in the queue,
+ *               return undefined
  *   length()  returns current list length
  */
 
 class Queue {
 
     constructor() {
+        this.qLength = 0;
     }
 
     add(item) {
+        this.qLength++;
+    }
+
+    remove() {
+        if (this.qLength === 0) {
+            return undefined;
+        }
+        this.qLength--;
+    }
+
+    length() {
+        return this.qLength;
     }
 }
 
--- queue.test.js
+++ queue.test.js
@@ -6,7 +6,31 @@ describe("test the length method", () => {
     const q = new Queue();
 
     test("list initially empty", () => {
-        expect(q.length().toBe(0));
+        expect(q.length()).toBe(0);
+    });
+
+    test("add increases length", () => {
+        q.add('test');
+        expect(q.length()).toBe(1);
+        q.add('test');
+        expect(q.length()).toBe(2);
+        q.add('test');
+        expect(q.length()).toBe(3);
+        q.add('test');
+        expect(q.length()).toBe(4);
+    });
+
+    test("remove decreases length", () => {
+        q.remove();
+        expect(q.length()).toBe(3);
+        q.remove();
+        expect(q.length()).toBe(2);
+        q.remove();
+        expect(q.length()).toBe(1);
+        q.remove();
+        expect(q.length()).toBe(0);
+        q.remove();
+        expect(q.length()).toBe(0);
     });
 
 });
