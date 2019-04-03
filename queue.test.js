ad91d15bb9231d68c80bb45c318755066921c4fe
Aleksander Budzynowski <998324+aleksb@users.noreply.github.com>
2019-04-03 14:14:08 +1100

    more length tests

--- queue.test.js
+++ queue.test.js
@@ -33,4 +33,30 @@ describe("test the length method", () => {
         expect(q.length()).toBe(0);
     });
 
+    test("add increases length", () => {
+        q.add('test');
+        expect(q.length()).toBe(1);
+        q.add('test');
+        expect(q.length()).toBe(2);
+        q.add('test');
+        expect(q.length()).toBe(3);
+        q.add('test');
+        expect(q.length()).toBe(4);
+    });
+
+    test("remove decreases length", () => {
+        q.remove();
+        expect(q.length()).toBe(3);
+        q.remove();
+        expect(q.length()).toBe(2);
+        q.remove();
+        expect(q.length()).toBe(1);
+        q.remove();
+        expect(q.length()).toBe(0);
+        q.remove();
+        expect(q.length()).toBe(0);
+    });
+
+});
+
 });
